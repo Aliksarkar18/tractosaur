6a56aee59604d68337ccec688d26d9cfbaf7950c
rad-ian <janek.radlinski@gmail.com>
2022-10-20 19:23:57 +0200

    add collision

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+const tractorElement = document.getElementById("tractor");
+const ladybugElement = document.getElementById("ladybug");
+
+function elementsOverLap(element1, element2) {
+    const rect1 = element1.getBoundingClientRect()
+    const rect2 = element2.getBoundingClientRect()
+
+    return rect1.right > rect2.left && rect1.left < rect2.right && rect1.top < rect2.bottom && rect1.bottom > rect2.top
+
+}
+
 window.onload = function () {
     document.onclick = jump
+    window.requestAnimationFrame(update)
 }
 
 function jump() {
-    const tractorElement = document.getElementById("tractor");
     if (!tractorElement.classList.contains("jump")) {
         tractorElement.classList.add("jump");
 
@@ -12,3 +23,13 @@ function jump() {
         }, 2000);
     }
 }
+
+function update() {
+    if (elementsOverLap(tractorElement, ladybugElement)) {
+        tractorElement.style.display = "none"
+        alert("nyny zjadło tratora")
+    }
+
+    window.requestAnimationFrame(update)
+}
+
